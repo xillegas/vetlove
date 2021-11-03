12f4b81f945944207fb766d9d8896ff7b6727b1b
Fernando Amundaray <fernandojal.une@gmail.com>
2021-11-03 12:49:40 -0400

    Completed CRUD Pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,10 @@
+SPECIES = ["Dog", "Cat", "Turtle", "Rabbit", "Hamster", "Bird", "Fish", "Ferret", "Snake", "Guinea pig" ]
+GENDERS = %w[Male Female]
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :gender, inclusion: GENDERS, presence: true
+  validates :species, inclusion: SPECIES, presence: true
+  validates :birthdate, presence: true
+  validates :name, presence: true
 end
