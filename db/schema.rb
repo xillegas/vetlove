8466e30fff7bf15347be92006bca78a2e7bb2786
Fernando Amundaray <fernandojal.une@gmail.com>
2021-11-04 18:29:08 -0400

    Geocode and mapbox init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_200119) do
+ActiveRecord::Schema.define(version: 2021_11_04_211916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 2021_11_02_200119) do
     t.string "state"
     t.string "municipality"
     t.string "parish"
-    t.float "lat"
-    t.float "lon"
+    t.float "latitude"
+    t.float "longitude"
     t.time "init_hour_day"
     t.time "end_hour_day"
     t.string "week_days"
