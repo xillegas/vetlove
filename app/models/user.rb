9b869f7cb83f4174bc9732f0cd0870ba46474b83
genesis fernandez <gensdugarte@gmail.com>
2021-11-05 12:35:24 -0400

    cloudinary funcionando

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one_attached :photo
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  :recoverable, :rememberable, :validatable
+  
   # Relaciones
   has_one :consulting_room
   has_many :pets
+  has_one_attached :photo
   # Validar con pundit el acceso en función de is_vet
 end
