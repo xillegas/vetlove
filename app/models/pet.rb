c44fd28e4b954664b2be0c9f737d23d7fe15350d
Alfredo Alejandro Villegas <xillegas@gmail.com>
2021-11-05 19:50:05 -0400

    Fusion realizada

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,16 @@ GENDERS = ['Macho', 'Hembra']
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :gender, inclusion: GENDERS, presence: true
+  validates :species, inclusion: SPECIES, presence: true
+  validates :birthdate, presence: true
+  validates :name, presence: true
+  validate :birthdate_lower_than_actual_date
+
+  def birthdate_lower_than_actual_date
+    return if self.birthdate.blank?
+    if self.birthdate >= Date.current.to_s
+      errors.add(:birthdate, "Birthdate can't be greather than today date")
+    end
+  end
 end
