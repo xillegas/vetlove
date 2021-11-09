3c2cb800f7ef90a34db4c5059454294f7688a0f1
Fernando Amundaray <fernandojal.une@gmail.com>
2021-11-09 13:12:07 -0400

    Geocoded by latitude and longitude

--- app/models/consulting_room.rb
+++ app/models/consulting_room.rb
@@ -2,7 +2,8 @@ class ConsultingRoom < ApplicationRecord
   has_many :bookings
   belongs_to :user
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :geocode, :reverse_geocode
   validates :name, uniqueness: true
 
   include PgSearch::Model
