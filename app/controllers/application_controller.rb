7815a0b77ae7b06f83f3b00709c58606ef29acf6
Alfredo Alejandro Villegas <xillegas@gmail.com>
2021-11-10 11:36:24 -0400

    Puliendo detalles finales de diseño

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   # end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || welcome_path
+    consulting_rooms_path
   end
 
   def remote_ip
